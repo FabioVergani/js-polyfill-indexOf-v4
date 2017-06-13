5971cad5d8bdecbf03944c26a133dd8307252319
Fabio Vergani <fabio.vergani.1975@gmail.com>
2017-06-13 18:19:37 +0200

    Create indexOf.js

--- indexOf.js
+++ indexOf.js
@@ -1,8 +1,11 @@
-;(function($){
-	var P=$.Array.prototype,M=$.Math;
-	P.indexOf||(P.indexOf=function(a,b){
-	 var i,e=Object(this),l=e.length>>>0,r=-1;
-	 if(l>0){i=M.abs(+b||0);if(i<l){for(i=M.max(0<=i?i:l-i,0);i<l;++i){if(i in e && e[i]===a){r=i;break}}}};
+(function($){
+ var o=$.Array.prototype,p='indexOf';
+ if(!(p in o && o[p])){
+	o[p]=function(a,b){
+	 var i,e=p(this),l=e.length>>>0,r=-1;
+	 if(l>0){i=o.abs(+b||0);if(i<l){for(i=o.max(0<=i?i:l-i,0);i<l;++i){if(i in e && e[i]===a){r=i;break}}}};
 	 return r;
-	});
+  };
+ o=$.Math,p=$.Object;
+ }
 })(window);
